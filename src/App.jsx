d70290316aa5e5d20a99d23920b15eec05baf587
maroofhaider <148911718+maroofhaider@users.noreply.github.com>
2023-10-25 02:36:29 +0500

    First commit react with sass tamplate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MhIntro from './components/MhIntro.jsx'
+import '/public/assets/css/Style.css'
+
+function App() {
+  return (
+    <MhIntro />
+  )
+}
+
+export default App
---
+++ src/components/MhIntro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MhIntro() {
+  return (
+    <div className="mh-intro">
+        <img src="/assets/images/favicon.svg" alt="" />
+        <h1>React integration framework through SASS projects 
+          <small>Check out <a href="https://maroofhaider.com/" target="_blank">maroofhaider.com</a></small>
+      </h1>
+    </div>
+  )
+}
+
+export default MhIntro
